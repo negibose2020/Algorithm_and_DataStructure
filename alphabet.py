453832e3b6daca46ccad52b79670ac2cdfaf3502
negibose2020 <allium.bose@gmail.com>
2023-05-02 23:30:43 +0000

    微修正

--- alphabet.py
+++ alphabet.py
@@ -1,17 +1,21 @@
+A=ord('A')
+print(A)    # 65
+
 alphabet = []
 for i in range(26):
     alphabet.append(chr(65+i))
 print(alphabet)
 # ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+
+
+a=ord('a')
+print(a)    # 97
+
 alphabet = []
 for i in range(26):
     alphabet.append(chr(97+i))
 print(alphabet)
+# ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-
-A=ord('A')
-print(A)    # 65
-a=ord('a')
-print(a)    # 97
