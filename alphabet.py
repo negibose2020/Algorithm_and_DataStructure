b803fabc986567996518c1b717a3054cc15db629
negibose2020 <allium.bose@gmail.com>
2023-05-02 23:29:51 +0000

    1st commit

---
+++ alphabet.py
@@ -0,0 +1,17 @@
+alphabet = []
+for i in range(26):
+    alphabet.append(chr(65+i))
+print(alphabet)
+# ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+alphabet = []
+for i in range(26):
+    alphabet.append(chr(97+i))
+print(alphabet)
+
+
+
+A=ord('A')
+print(A)    # 65
+a=ord('a')
+print(a)    # 97
