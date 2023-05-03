1a08c127549a2dffc753b03ef97e24bdfa82b368
negibose2020 <allium.bose@gmail.com>
2023-05-03 22:38:00 +0000

    1st commit

---
+++ RollingHash.py
@@ -0,0 +1,24 @@
+class RollingHash():
+    def __init__(self, S, base = 317, p = 1<<61 -1):
+        self.S = S
+        self.N = len(self.S)
+        self.base = base
+        self.mod = p
+        self.r_hash = 0
+        self.poshash = dict()
+        self.pow_base = [1]
+        self.poshash[0]=0
+        for i in range(self.N):
+            self.r_hash *= self.base
+            self.r_hash += ord(self.S[i])
+            self.r_hash %= self.mod
+            self.poshash[i+1] = self.r_hash
+            self.pow_base.append((self.pow_base[-1] * self.base) % self.mod)
+    
+    def get(self, l, r):
+        """
+        return  :   hash value of str[l:r)
+        """
+        res = self.poshash[r] - self.poshash[l]*self.pow_base[r-l]
+        res %= self.mod
+        return res
