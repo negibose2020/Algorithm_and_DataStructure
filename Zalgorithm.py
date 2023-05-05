8f3c2465c8f578a3e56322a7de3b1de1631279bc
negibose2020 <allium.bose@gmail.com>
2023-05-05 06:03:17 +0000

    1st commit

---
+++ Zalgorithm.py
@@ -0,0 +1,23 @@
+def Zalgorithm(S):
+    N = len(S)
+    A = [0]*N
+    A[0] = N
+    i, j = 1, 0
+    while i < N:
+        while i+j < N and S[j]==S[i+j]:
+            j += 1
+        A[i] = j
+        if j == 0:
+            i += 1
+            continue
+        k = 1
+        while i+k < N and k + A[k] < j :
+            A[i+k] = A[k]
+            k += 1
+        i += k
+        j -= k
+    return A
+
+s=input()
+a=Zalgorithm(s)
+print(*a)
