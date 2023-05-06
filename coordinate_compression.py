e6c025d1dd1a27fe5e99ae3c757568e326eeedf1
negibose2020 <allium.bose@gmail.com>
2023-05-06 23:32:36 +0000

    1st commit

---
+++ coordinate_compression.py
@@ -0,0 +1,10 @@
+def coordinate_compression(ls):
+    sortedls = sorted(set(ls))
+    dic = {v:i for i,v in enumerate(sortedls)}
+    # return dic
+    return list(map(lambda x:dic[x], ls))
+
+a = [0,10,4,405,2,555,50200,340,2,10]
+
+ca=coordinate_compression(a)
+print(ca)
