8fe8d657d7bd80ddaa3606ff0de4df32efd9fa2d
negibose2020 <allium.bose@gmail.com>
2023-05-09 14:32:18 +0000

    pass アルゴリズムと数学　演習問題集   013 - Divisor Enumeration

--- enumerate_divisors.py
+++ enumerate_divisors.py
@@ -8,3 +8,9 @@ def enumerate_divisors(N):
             res.add(N//i)
     return sorted(res)
 
+# pass アルゴリズムと数学　演習問題集   013 - Divisor Enumeration
+# https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_m
+
+n=int(input())
+ans=enumerate_divisors(n)
+print(*ans)
